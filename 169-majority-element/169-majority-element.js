e3249dedc0f4e02d61baba89d7165abadb4baa07
Louisa Yonzon <80828843+Louisaflor@users.noreply.github.com>
2022-09-20 16:01:31 -0700

    Time: 134 ms (13.70%), Space: 43.7 MB (69.26%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -2,43 +2,43 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var majorityElement = function(nums) {
-//     if (nums.length === 1) {
-//         return 1
-//     }
-//     //create obj to store all counted elements
-//     //store the highest count element 
+var majorityElement = function(nums) {
+    if (nums.length === 1) {
+        return 1
+    }
+    //create obj to store all counted elements
+    //store the highest count element 
     
-//     //iterate over the nums array
-//     //check if the nums element is inside the obj
-//     //if not, create one and put 1 as a value
-//     //if yes, increment the count 
-//     //check which one has the highest num in obj
+    //iterate over the nums array
+    //check if the nums element is inside the obj
+    //if not, create one and put 1 as a value
+    //if yes, increment the count 
+    //check which one has the highest num in obj
     
   
-//     //return the element 
+    //return the element 
     
     
-//     var storeCount = {}
-//     var highiestCountElement = nums[0]
+    var storeCount = {}
+    var highiestCountElement = nums[0]
     
-//     for (var i = 0; i < nums.length; i++) {
-//         var index = nums[i]
-//         if (storeCount[index]) {
-//             storeCount[index]++ 
-//             console.log("WHAT IS THE INDEX AFTER COUNTING: ", storeCount[index])
-//             if (storeCount[index] > storeCount[highiestCountElement]) {
-//                 highiestCountElement = index
-//             } 
-//         } else {
-//                 storeCount[index] = 1
-//             }
-//     }
-//     console.log("STORE OBJ: ", storeCount)
+    for (var i = 0; i < nums.length; i++) {
+        var index = nums[i]
+        if (storeCount[index]) {
+            storeCount[index]++ 
+            //console.log("WHAT IS THE INDEX AFTER COUNTING: ", storeCount[index])
+            if (storeCount[index] > storeCount[highiestCountElement]) {
+                highiestCountElement = index
+            } 
+        } else {
+                storeCount[index] = 1
+            }
+    }
+    //console.log("STORE OBJ: ", storeCount)
     
-//     return highiestCountElement
+    return highiestCountElement
     
-// };
+};
 
 
 
@@ -82,27 +82,27 @@ e:??
 
 
 
-var majorityElement = function(nums) {
-    var countObj = {}
-    var numsLength = nums.length / 2
+// var majorityElement = function(nums) {
+//     var countObj = {}
+//     var numsLength = nums.length / 2
     
-    var mostCount = 0;
-    var number = nums[0]
+//     var mostCount = 0;
+//     var number = nums[0]
     
-    for (var i = 0; i < nums.length; i++) {
-        var curr = nums[i]
-        if (countObj[curr]) {
-            countObj[curr]++
-        } else {
-            countObj[curr] = 1
-        }
+//     for (var i = 0; i < nums.length; i++) {
+//         var curr = nums[i]
+//         if (countObj[curr]) {
+//             countObj[curr]++
+//         } else {
+//             countObj[curr] = 1
+//         }
         
-        if (countObj[curr] > mostCount) {
-            mostCount = countObj[curr]
-            number = curr
-        }
-    }
+//         if (countObj[curr] > mostCount) {
+//             mostCount = countObj[curr]
+//             number = curr
+//         }
+//     }
     
-    return number;
+//     return number;
     
-}
+// }
