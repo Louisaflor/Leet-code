9796135f215fa319eb6c89d8797937ae1dccfb31
Louisa Yonzon <80828843+Louisaflor@users.noreply.github.com>
2022-09-29 15:48:43 -0700

    Time: 111 ms (80.57%), Space: 51.6 MB (22.20%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,54 +2,25 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-
-/*
-i: aray of numbers 
-o: boolean, if there are duplicate numbers in the array 
-c:?? 
-e:?? 
-
-//create an empty arr to store non duplicate num
-//iterate over the nums array
-//check if that char is in the arr
-//if not, add into arr
-//if yes, return true
-*/
-// var containsDuplicate = function(nums) {
-//     var nonDupArr = []
-    
-//     for (var i = 0; i < nums.length; i++) {
-//         var char = nums[i]
-//         if (nonDupArr.includes(char)) {
-//             return true
-//         }
-        
-//         nonDupArr.push(char)
-//     }
-    
-//     return false 
-    
-// };
-
-//create a new Set
-//iterate over the nums array
-//add in the char to the set
-//check if the set is the same as the nums arr
-//if yes, return false 
-//otherwise return true
-
 var containsDuplicate = function(nums) {
-    var nonDup = new Set()
+    //store the num counts in a object key: num, value: counter
+    
+    //iterate over the nums and check if the curr obj has a value 
+    //if yes return true
+    
+    //if done iterating just return false 
+    
+    var countObj = {}
     
     for (var i = 0; i < nums.length; i++) {
-        nonDup.add(nums[i])
+        var currNum = nums[i]
+        if (countObj[currNum]) {
+            return true
+        } else {
+            countObj[currNum] = 1
+        }
     }
     
-    if (nonDup.size === nums.length) {
-        return false 
-    } else {
-        return true
-    }
-   
+    return false
     
 };
